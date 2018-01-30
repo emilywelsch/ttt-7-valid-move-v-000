bcf4426985a7181da9f71635e9d9186d63377888
Emily Welsch <emwelsch@gmail.com>
2018-01-30 15:16:45 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -9,7 +9,6 @@ def valid_move?(board,index)
     puts "true"
   end
   else puts "nil"
-  end
 end
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 board = []
