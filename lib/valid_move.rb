757a4abb22a045a81a6ebff2f3d35d6f3779f49b
Emily Welsch <emwelsch@gmail.com>
2018-01-30 14:55:07 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,8 @@
 # code your #valid_move? method here
+board = []
+index = 0
 def valid_move?(board,index)
-  if index.between?(0,8) == 0 && position_taken?(board,index) != 0
+  if index.between?(0,8) == 0 && position_taken?(board,index) == 0
   else puts "nil"
   end
 end
