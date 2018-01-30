a80136f056e4bcb5750964821f2747bb033c7b1c
Emily Welsch <emwelsch@gmail.com>
2018-01-30 15:52:57 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,9 +2,8 @@
 board = []
 index = 0
 def valid_move?(board,index)
-  if index.between?(0,8) == 0 && position_taken?(index) != 0
+  if index.between?(0,8) == 0 && position_taken?(index) == 0
     return true
-  end
   else
     return false
   end
